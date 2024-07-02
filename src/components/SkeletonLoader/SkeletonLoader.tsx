c2e969084c25f3c5cb1a5e52d7825ee0bdfc6c06
Syed Muzammil <syyedmuzammilshah6@gmail.com>
2024-07-03 01:31:10 +0500

    create skeleton loader component

---
+++ src/components/SkeletonLoader/SkeletonLoader.tsx
@@ -0,0 +1,24 @@
+import './SkeletonLoader.scss';
+
+interface SkeletonLoaderProps {
+  width?: string;
+  height?: string;
+  maxHeight?: string;
+}
+
+const SkeletonLoader = ({
+  width = '100%',
+  height = '250px',
+  maxHeight = '100%',
+}: SkeletonLoaderProps) => {
+  return (
+    <div
+      className="skeleton-loader"
+      style={{ width, minWidth: '100%', height, maxHeight }}
+    >
+      <div className="skeleton-content" />
+    </div>
+  );
+};
+
+export default SkeletonLoader;
